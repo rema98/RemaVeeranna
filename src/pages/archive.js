444dc0e2473902d93998f8697bad45d1a0301b95
rema98 <39127803+rema98@users.noreply.github.com>
2022-01-15 13:12:39 -0500

    archive.js commited

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -110,13 +110,13 @@ const ArchivePage = ({ location, data }) => {
     <Layout location={location}>
       <Helmet>
         <title>Archive | Chandrika Deb</title>
-        <link rel="canonical" href="https://chandrikadeb7.github.io/archive" />
+        <link rel="canonical" href="https://remaveeranna.gatsbyjs.io/archive" />
       </Helmet>
 
       <StyledMainContainer>
         <header ref={revealTitle}>
           <h1 className="big-title">Archive</h1>
-          <p className="subtitle">A big list of things I’ve worked on</p>
+          <p className="subtitle">A list of things I’ve worked on</p>
         </header>
 
         <StyledTableContainer ref={revealTable}>
